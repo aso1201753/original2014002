d0a795d98c2553d5173d1ceb8dde1b3d4023887b
student <student@PC111S36.fgakusei.local>
2014-06-13 11:13:46 +0900

    じょうしせん　3B　末延　内部DB　未完成品

---
+++ src/jp/ac/st/asojuku/original2014002/MainActivity.java
@@ -0,0 +1,94 @@
+package jp.ac.st.asojuku.original2014002;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+	SQLiteDatabase sdb = null;
+	MySQLiteOpenHelper helper = null;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+
+
+	@Override
+	protected void onResume() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onResume();
+
+		Button btnENTRY = (Button)findViewById(R.id.btnENTRY);
+		btnENTRY.setOnClickListener(this);
+
+		Button btnMAINTE = (Button)findViewById(R.id.btnMAINTE);
+		btnMAINTE.setOnClickListener(this);
+
+		Button btnCHECK = (Button)findViewById(R.id.btnCHECK);
+		btnCHECK.setOnClickListener(this);
+
+		if(sdb == null) {
+
+			helper = new MySQLiteOpenHelper(getApplicationContext());
+		}
+
+		try{
+			sdb = helper.getWritableDatabase();
+		}catch(SQLiteException e){
+
+			return;
+		}
+
+
+		}
+	@Override
+	public void onClick(View v) {
+
+		Intent intent = null;
+		switch(v.getId()) {
+		case R.id.btnENTRY:
+			EditText etv = (EditText)findViewById(R.id.edtMsg);
+			String inputMsg = etv.getText().toString();
+
+			if(inputMsg!=null && !inputMsg.isEmpty()){
+
+				helper.insertHitokoto(sdb, inputMsg);
+			}
+
+		etv.setText("");
+		break;
+		case R.id.btnMAINTE:
+
+
+		intent = new Intent(MainActivity.this, HitokotoActivity.class);
+		intent.putExtra("hitokoto", strHitokoto);
+
+		startActivity(insert);
+		break;
+		}
+
+
+	}
+
+
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
+
